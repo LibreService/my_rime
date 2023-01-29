f0bcb3082cd37a9375c8124d0c6eb4e27050dc9b
Qijia Liu <liumeo@pku.edu.cn>
2023-01-28 21:29:24 -0500

    librime: upgrade to 1.8.3

--- scripts/build_native.sh
+++ scripts/build_native.sh
@@ -21,5 +21,6 @@ make -C build/librime_native -j $n
 
 rime_dir=build/librime_native/bin plum/rime-install prelude essay
 node scripts/install_schemas.mjs
+rm -rf public/ime
 cp -r build/librime_native/bin/build public/ime
 rm public/ime/default.yaml
